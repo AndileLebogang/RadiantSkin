a8333d07add5a3c97cee93d899c55ef2de36e429
nonhlanhlahazel22 <222687290@mycput.ac.za>
2026-06-21 20:44:41 +0200

    Updated Admin and User entities

--- src/main/java/ac/za/mycput/domain/Admin.java
+++ src/main/java/ac/za/mycput/domain/Admin.java
@@ -8,21 +8,47 @@ public class Admin extends User {
     private String employeeNumber;
 
     protected Admin() {
-
     }
 
-    public Admin(Long userId, String firstName, String lastName, String email,
-                 String password, String employeeNumber) {
-        super(userId, firstName, lastName, email, password);
-        this.employeeNumber = employeeNumber;
+    private Admin(Builder builder) {
+        super(builder);
+        this.employeeNumber = builder.employeeNumber;
     }
 
     public String getEmployeeNumber() { return employeeNumber; }
 
     @Override
     public String toString() {
-        return "Admin{" +
-                "employeeNumber='" + employeeNumber + '\'' +
-                "} " + super.toString();
+        return "Admin{" + super.toString() +
+                ", employeeNumber='" + employeeNumber + '\'' +
+                '}';
+    }
+
+    public static class Builder extends User.Builder<Builder> {
+        private String employeeNumber;
+
+        public Builder setEmployeeNumber(String employeeNumber) {
+            this.employeeNumber = employeeNumber;
+            return this;
+        }
+
+        @Override
+        public Builder copy(User user) {
+            super.copy(user);
+            if (user instanceof Admin) {
+                Admin admin = (Admin) user;
+                this.employeeNumber = admin.employeeNumber;
+            }
+            return this;
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+        public Admin build() {
+            return new Admin(this);
+        }
     }
 }
--- src/main/java/ac/za/mycput/domain/User.java
+++ src/main/java/ac/za/mycput/domain/User.java
@@ -7,22 +7,22 @@ import jakarta.persistence.*;
 public abstract class User {
 
     @Id
-    private Long userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
+    protected Long userId;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String password;
 
     protected User() {
 
     }
 
-    public User(Long userId, String firstName, String lastName, String email, String password) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
+    protected User(Builder<?> builder) {
+        this.userId = builder.userId;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.password = builder.password;
     }
 
     public Long getUserId() { return userId; }
@@ -33,11 +33,49 @@ public abstract class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "userId=" + userId +
+        return "userId=" + userId +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+                ", email='" + email + '\'';
+    }
+
+    public static abstract class Builder<T extends Builder<T>> {
+        private Long userId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+
+        public T setUserId(Long userId) {
+            this.userId = userId;
+            return self();
+        }
+        public T setFirstName(String firstName) {
+            this.firstName = firstName;
+            return self();
+        }
+        public T setLastName(String lastName) {
+            this.lastName = lastName;
+            return self();
+        }
+        public T setEmail(String email) {
+            this.email = email;
+            return self();
+        }
+        public T setPassword(String password) {
+            this.password = password;
+            return self();
+        }
+
+        public T copy(User user) {
+            this.userId = user.userId;
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+            this.email = user.email;
+            this.password = user.password;
+            return self();
+        }
+
+        protected abstract T self();
     }
 }
